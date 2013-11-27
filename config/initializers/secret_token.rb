df09446f76b9586b01a8e4aa76c5f0722ff73aec
Senchamatcha <info@senchamatcha.com>
2013-11-27 08:34:50 +0000

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'a3400e02cc3f4bf3c360417fa079ae42c1921c7702e07a3f422f02589a9babdef0ce910ebb6f55da1a8399c9f93637bb5eb6a73048c02dd5e60b59f2c3072e76'
